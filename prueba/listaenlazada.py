ff15d863bea5a8099aba2995a3621784a0372a77
dg203302 <diegogarcia479@yahoo.com.ar>
2024-05-17 11:23:45 -0300

    "lkdlst bien"

--- prueba/listaenlazada.py
+++ prueba/listaenlazada.py
@@ -1,17 +1,6 @@
 import csv
+from nodo import nodo
 from subclase import *
-class nodo:
-    __datos:object
-    __siguiente:object
-    def __init__(self,datos=None,siguiente=None):
-        self.__datos=datos
-        self.__siguiente=siguiente
-    def actsig(self,siguiente):
-        self.__siguiente=siguiente
-    def getsig(self):
-        return self.__siguiente
-    def __str__(self):
-        return f'{self.__datos}'
 class linkdlist:
     __cabe:object
     def __init__(self):
