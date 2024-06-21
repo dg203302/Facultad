1d9f0383001f08c089f3dea0275d35ad07830225
Diego <diegogarcia479@yahoo.com.ar>
2024-06-21 17:11:57 -0300

    funcionalidad 2 hecha

--- PRACTICOS/Practico Flask/appweb.py
+++ PRACTICOS/Practico Flask/appweb.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask,request,render_template,session
 from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
@@ -10,13 +11,27 @@ def inicio():
 def accesodesp():
     if request.method=='POST':
         session['sucursal_seleccionada']=request.form.get('sucursales')
-        return render_template('funcionesdespachante.html', sucursal=db.session.query(Sucursal).filter(Sucursal.id==session['sucursal_seleccionada']).first())
+        return render_template('funcionesdespachante.html', sucursal=db.session.query(Sucursal).filter(Sucursal.id==session['sucursal_seleccionada']).first()) #ineficiente
     else:
         return render_template('accesodesp.html', sucursales=db.session.query(Sucursal).all(), sucursal_seleccionada=None)
-@app.route('/registrarrecepcion')
+@app.route('/registrarrecepcion', methods=['GET','POST'])
 def registrarrecepcion():
-    return 'en proceso'
-#@app.route('/registrarsalida')
+    if request.method=='POST':
+        numerodeenvio=random.randint(0,1000)
+        peso=request.form.get('peso')
+        nombre=request.form.get('nombre')
+        direccion=request.form.get('direccion')
+        sucu=db.session.query(Sucursal).filter(Sucursal.id==session['sucursal_seleccionada']).first() #ineficiente
+        nuevopaquete=Paquete(numeroenvio=numerodeenvio,peso=peso,nomdestinatario=nombre,dirdestinatario=direccion,observaciones='',idsucursal=sucu.id)
+        db.session.add(nuevopaquete)
+        db.session.commit()
+        return render_template('registrocorrecto.html', paquete=nuevopaquete)
+    else:
+        return render_template('registrarrecepcionpaquete.html', sucu=db.session.query(Sucursal).filter(Sucursal.id==session['sucursal_seleccionada']).first())
+@app.route('/registrarsalida', methods=['POST','GET'])
+def registrarsalida():
+    
+    return
 #@app.route('/regsitrarllegada')
 if __name__=='__main__':
     with app.app_context():
