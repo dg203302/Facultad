2facf9fa85fe4e06793262a23b2be95320a28c80
Diego <diegogarcia479@yahoo.com.ar>
2024-06-24 13:50:31 -0300

    cambios minimos

--- parcial 2 luka/electricas.py
+++
@@ -1,8 +0,0 @@
-from Herramientas import *
-class electricas(herramientas):
-    __tipo_elec:str
-    def __init__(self, marc, mode, an, tipo, pot, capa, tari, canti, tipoele):
-        super().__init__(marc, mode, an, tipo, pot, capa, tari, canti)
-        self.__tipo_elec=tipoele
-    def __str__(self):
-        return 'soy un herramienta electrica!'
