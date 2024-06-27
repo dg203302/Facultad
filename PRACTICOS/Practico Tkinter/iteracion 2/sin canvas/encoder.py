2c86215788f4747d7c6435e2809e4271b19ce84d
Diego <diegogarcia479@yahoo.com.ar>
2024-06-26 22:36:22 -0300

    Reorden de carpetas

--- PRACTICOS/Practico Tkinter/iteracion 2/sin canvas/encoder.py
+++
@@ -1,6 +0,0 @@
-import json
-class encoder:
-    def tojson(self,jugadores):
-        with open('PRACTICOS/Practico Tkinter/iteracion 2/sin canvas/pysimonpuntajes.json', mode='a') as guard:
-            json.dump(jugadores,guard,indent=4)
-            guard.close()
---
+++ Practica Unidad 2/punto 1/punto 1/MAIN PUNTO 1.py
@@ -0,0 +1,16 @@
+from CAJAAHORRO import caja_ahorro
+def test():
+    nume=input('ingrese el numero de cuenta: ')
+    Cuil=input('ingrese el cuil: ')
+    Ape=input('ingrese el apellido: ')
+    Nom=input('ingrese el nombre: ')
+    Sald=float(input('ingrese el saldo: '))
+    cuent1=caja_ahorro(nume,Cuil,Ape,Nom,Sald)
+    cuent1.valcuil()
+    cuent1.mostrardat()
+    impor=float(input('ingrese el importe que desee extraer: '))
+    cuent1.extimpt(impor)
+    impdep=float(input('ingrese el importe que desee depositar: '))
+    cuent1.dep(impdep)
+if __name__ == '__main__':
+    test()
