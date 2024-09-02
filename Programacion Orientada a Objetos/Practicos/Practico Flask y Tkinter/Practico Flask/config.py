fd56312fddd4babcedfd65ecb5fb18303f0c8f18
Diego <diegogarcia479@yahoo.com.ar>
2024-09-01 23:47:30 -0300

    nada importante

--- Programacion Orientada a Objetos/Practicos/Practico Flask y Tkinter/Practico Flask/config.py
+++ Programacion Orientada a Objetos/Practicos/Practico Flask y Tkinter/Practico Flask/config.py
@@ -1,4 +1,3 @@
 SECRET_KEY='DGgd203302'
-#SQLALCHEMY_DATABASE_URI='sqlite:///C:/Users/Lolma/Documents/Codigos/Python/POO/Practicos/Practico Flask y Tkinter/Practico Flask/datos.sqlite3' #base de datos para pc
-SQLALCHEMY_DATABASE_URI='sqlite:///C:/Users/Lolma/Documents/Codigos/POO/Practicos/Practico Flask y Tkinter/Practico Flask/datos.sqlite3' #base de datos para notebook
+SQLALCHEMY_DATABASE_URI='C:/Users/Lolma/Documents/Codigos y Proyectos/Programacion Orientada a Objetos/Practicos/Practico Flask y Tkinter/Practico Flask/datos.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS= False
