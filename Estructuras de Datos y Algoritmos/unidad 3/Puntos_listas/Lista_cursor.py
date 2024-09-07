ae378d51691961045ebe41d47f746d43b1fdfd2e
Diego <diegogarcia479@yahoo.com.ar>
2024-09-07 00:11:17 -0300

    lista por cursor finalmente bien

--- Estructuras de Datos y Algoritmos/unidad 3/Puntos_listas/Lista_cursor.py
+++ Estructuras de Datos y Algoritmos/unidad 3/Puntos_listas/Lista_cursor.py
@@ -18,49 +18,48 @@ class lista_cursor:
     __disponible:int
     def __init__(self,dimension=0):
         self.__dimension=dimension
+        self.__elementos=[None]*self.__dimension
         self.__cantidad_elementos=0
-        self.__disponible=0
+        self.__disponible=-1
         self.__primer_elemento=0
-        self.__elementos=[nodo_cursor()]*self.__dimension
     def buscar_vacio(self):
-        if self.__cantidad_elementos==0:
-            self.__disponible=self.__primer_elemento
-            return True
-        else:
-            i=0
-            while i<self.__cantidad_elementos and self.__elementos[i].get_siguiente()!=-1:
-                i+=1
-            if self.__elementos[i].get_siguiente()==-1:
-                self.__disponible=i+1
+        i=0
+        while i<self.__dimension:
+            if self.__elementos[i]==None:
+                self.__disponible=i
                 return True
-            else:
-                return False
+            i+=1
+        return False
     def insertar_en_posicion(self,dato,indice_insertar):
         if self.__cantidad_elementos<self.__dimension and self.buscar_vacio():
             nodo_a_insertar=nodo_cursor(dato)
             self.__elementos[self.__disponible]=nodo_a_insertar
             if self.__cantidad_elementos==0:
-                self.__cantidad_elementos+=1              #primer elemento, independientemente del indice
+                self.__primer_elemento=self.__disponible
+                self.__cantidad_elementos+=1
+            elif indice_insertar==0:
+                self.__elementos[self.__disponible].set_siguiente(self.__primer_elemento)
+                self.__primer_elemento=self.__disponible
+                self.__cantidad_elementos+=1
             else:
-                i=self.__primer_elemento
-                while i<indice_insertar:
-                    i+=1
-                if i==indice_insertar:
-                    print(f'indice de la busqueda: {i}, indice del insertado: {self.__disponible}')
-                    if i==self.__disponible or self.__elementos[i].get_dato()==None:                  #al final
-                        self.__elementos[i-1].set_siguiente(self.__disponible)
-                        self.__cantidad_elementos+=1
-                    elif i!=self.__disponible or self.__elementos[i]!=None:                           #en el medio
-                        if i==0:                                                                      #caso particular de que sea al inicio
-                            self.__elementos[self.__disponible].set_siguiente(self.__primer_elemento)
-                            self.__primer_elemento=self.__disponible
-                            self.__cantidad_elementos+=1
-                        else:                                                                         #en el medio ahora si
-                            self.__elementos[self.__disponible].set_siguiente(i)
-                            self.__elementos[i-1].set_siguiente(self.__disponible)
-                            self.__cantidad_elementos+=1    
+                actual=self.__primer_elemento
+                for j in range(indice_insertar-1):
+                    actual=self.__elementos[actual].get_siguiente()
+                nodo_a_insertar.set_siguiente(self.__elementos[actual].get_siguiente())
+                self.__elementos[actual].set_siguiente(self.__disponible)
+                self.__cantidad_elementos+=1
         else:
             print('lista llena!')
+    def recorrer(self):
+        if self.__cantidad_elementos!=0:
+            cabeza = self.__primer_elemento
+            print("Lista:", end=" ")
+            while cabeza!=-1:
+                print(self.__elementos[cabeza].get_dato(), end=" ")
+                cabeza=self.__elementos[cabeza].get_siguiente()
+            print()
+        else:
+            print("Lista vacía.")
     def recorrer_rudimentario(self):
         i=self.__primer_elemento
         while self.__elementos[i].get_siguiente()!=-1:
@@ -68,10 +67,11 @@ class lista_cursor:
             i=self.__elementos[i].get_siguiente()
         print(f'CONTENIDO: {self.__elementos[i].get_dato()}\nSIGUIENTE: {self.__elementos[i].get_siguiente()}\ncantidad de elementos: {self.__cantidad_elementos}') #esto para que muestre el ultimo
 if __name__=='__main__':
-    '''
-    lista_prueba=lista_cursor(3)
+    
+    lista_prueba=lista_cursor(5)
     lista_prueba.insertar_en_posicion(21,0)
     lista_prueba.insertar_en_posicion(12,1)
     lista_prueba.insertar_en_posicion(31,0)
-    lista_prueba.recorrer_rudimentario()
-    '''
+    lista_prueba.insertar_en_posicion(231,2)
+    lista_prueba.recorrer()
+    
