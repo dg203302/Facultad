d460f44a5a1656061e9456b9331bab8853551660
Diego <168243435+dg203302@users.noreply.github.com>
2024-09-19 02:05:18 -0300

    .........

--- Estructuras de Datos y Algoritmos/unidad 3/Puntos_listas/concatenar_por_contenido.py
+++ Estructuras de Datos y Algoritmos/unidad 3/Puntos_listas/concatenar_por_contenido.py
@@ -1,4 +1,5 @@
 from Listas_por_contenido import *
+import time
 def concatenar(lista1, lista2, lista3):
     i=0
     while i<(lista1.get_cantidad()+lista2.get_cantidad()):
@@ -7,18 +8,34 @@ def concatenar(lista1, lista2, lista3):
         else:
             lista3.insertar(lista1.buscar(i))
         i+=1
+    #calculo de eficiencia
+    #1
+    #(n+m)-1
+        #(m+n)
+            #n+2
+        #n+1
+    #n+m
+    
+    #1+((n+m)-1)+(m+n)+(n+2)+(n+1)
+    #1+((n+m)-1)+(m+n)+n+2+n+1
+    #1+((n+m)-1)+m+n+2+n+1
+    #1+(n+m)+m+n+2+n
+    
+    #eficiencia final 3n+2m+3
+    
+    #O(n+m) aproximado O(n)
+
 if __name__ == '__main__':
     lista1=lista_enlazada()
     lista2=lista_enlazada()
     lista3=lista_enlazada()
-    lista1.insertar(1)
-    lista1.insertar(2)
-    lista1.insertar(3)
-    lista1.insertar(4)
-    lista1.insertar(5)
-    lista2.insertar(6)
-    lista2.insertar(7)
-    lista2.insertar(8)
-    lista2.insertar(10)
+    for i in range(1,253):
+        if i>(i/2):
+            lista2.insertar(i)
+        else:
+            lista1.insertar(i)
+    tiempo_inicial = time.time()
     concatenar(lista1, lista2, lista3)
+    tiempo_final = time.time()
     lista3.recorrer()
+    print("Tiempo de ejecución: ", tiempo_final-tiempo_inicial)
