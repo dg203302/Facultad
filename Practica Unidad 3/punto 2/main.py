f46f3759b4ead7bdd3c3c287f122f63a9352f851
Diego <168243435+dg203302@users.noreply.github.com>
2024-09-30 19:59:54 -0300

    reorden

--- Practica Unidad 3/punto 2/main.py
+++
@@ -1,9 +0,0 @@
-from gstladril import gestor_ladrillo
-from gstmat import gestor_materiales
-gstm=gestor_materiales()
-gstld=gestor_ladrillo(gstm)
-gstld.incisoa()
-gstld.incisob()
-gstld.incisoc()
-'''bien hecho segun como lo entendi.
-la agregacion como tal esta bien hecha'''
