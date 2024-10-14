1bf188ede874b9e6dbc560e414365a543172c402
Diego <168243435+dg203302@users.noreply.github.com>
2024-10-14 16:26:53 -0300

    hash bucket terminado

--- Estructuras de Datos y Algoritmos/unidad_5/Hashing/hashing_bucket.py
+++ Estructuras de Datos y Algoritmos/unidad_5/Hashing/hashing_bucket.py
@@ -10,18 +10,8 @@ class sub_lista:
     def verificar(self):
         return self.__cantidad_columnas == self.__tamano_columnas
     def cargar(self, valor):
-        if self.verificar():
-            i=int(self.__tamano_columnas * 0.2)
-            while i < self.__tamano_columnas:
-                if self.__items_columnas[i] is None:
-                    self.__items_columnas[i] = valor
-                    self.__cantidad_columnas += 1
-                    return
-                i += 1
-            return
-        else:
-            self.__items_columnas[self.__cantidad_columnas] = valor
-            self.__cantidad_columnas += 1
+        self.__items_columnas[self.__cantidad_columnas] = valor
+        self.__cantidad_columnas += 1
     def get_item(self, indice):
         return self.__items_columnas[indice]
     def get_cantidad(self):
@@ -31,21 +21,31 @@ class sub_lista:
 class hash_bucket:
     __items_filas: list
     __tamano_filas: int
+    __items_overflow: list
     __cantidad_filas: int
-
     def __init__(self, cantidad_elementos, tamano_columnas):
-        filas=int((cantidad_elementos / tamano_columnas) * 1.2)
+        filas=int(cantidad_elementos / tamano_columnas)
         self.__items_filas = [sub_lista(tamano_columnas) for _ in range(filas)]
+        self.__items_overflow= [sub_lista(tamano_columnas) for _ in range(int(filas*0.2))]
         self.__tamano_filas = filas
         self.__cantidad_filas = 0
-
     def hasheo_modulo(self, clave):
         return clave % self.__tamano_filas
-
     def insertar(self, valor):
         indice = self.hasheo_modulo(valor)
-        self.__items_filas[indice].cargar(valor)
-        self.__cantidad_filas += 1
+        if self.__items_filas[indice].verificar():
+            print('Overflow')
+            indice=0
+            while self.__items_overflow[indice].verificar():
+                indice+=1
+            if indice == len(self.__items_overflow):
+                print('Overflow lleno')
+            else:
+                self.__items_overflow[indice].cargar(valor)
+            return
+        else:
+            self.__items_filas[indice].cargar(valor)
+            self.__cantidad_filas += 1
     def buscar(self, valor):
         indice = self.hasheo_modulo(valor)
         i = 0
@@ -55,7 +55,6 @@ class hash_bucket:
             return False
         else:
             return True
-
     def recorrer(self):
         for i in range(self.__tamano_filas):
             cadena = '['
@@ -63,9 +62,16 @@ class hash_bucket:
                 cadena += str(self.__items_filas[i].get_item(j)) + ','
             cadena = cadena.rstrip(',') + ']'
             print(f'{i} --> {cadena}')
+        print('----------------Overflow----------------')
+        for i in range(len(self.__items_overflow)):
+            cadena = '['
+            for j in range(self.__items_overflow[i].get_cantidad()):
+                cadena += str(self.__items_overflow[i].get_item(j)) + ','
+            cadena = cadena.rstrip(',') + ']'
+            print(f'{i} --> {cadena}')
 #---------------------------------------------MAIN---------------------------------------------#
 if __name__=='__main__':
-    cantidad_elementos=10#int(input('Ingrese la cantidad de elementos: '))
+    cantidad_elementos=30#int(input('Ingrese la cantidad de elementos: '))
     cantidad_columnas=3#int(input('Ingrese la cantidad de columnas: '))
     hasheo_bucket = hash_bucket(cantidad_elementos, cantidad_columnas)
     #probar despues el overflow a ver si anda
